c595660d36e9a32dc31e6e08d53cf25a5330c2fb
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-06-27 23:43:03 -0400

    creating nav test file

---
+++ src/components/FixedNavBar/FixedNavBar.test.tsx
@@ -0,0 +1,14 @@
+import { FixedNavBar } from "./FixedNavBar";
+import {shallow} from "enzyme";
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = shallow(<FixedNavBar/>)
+})
+
+describe('<FixedNavBar/> render', () => {
+    test('<FixedNavBar/> snapshot',  () => {
+        
+    })
+})
