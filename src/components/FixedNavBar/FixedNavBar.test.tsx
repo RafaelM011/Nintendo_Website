36e8bc781babac9d10412749f0c3ec962eef65ee
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-06-28 00:12:49 -0400

    veryfing rendering

--- src/components/FixedNavBar/FixedNavBar.test.tsx
+++ src/components/FixedNavBar/FixedNavBar.test.tsx
@@ -1,14 +1,11 @@
 import { FixedNavBar } from "./FixedNavBar";
-import {shallow} from "enzyme";
-
-let wrapper;
-
-beforeEach(() => {
-    wrapper = shallow(<FixedNavBar/>)
-})
+import { render, screen } from "@testing-library/react";
 
 describe('<FixedNavBar/> render', () => {
-    test('<FixedNavBar/> snapshot',  () => {
-        
+    const {container} = render(<FixedNavBar/>);
+    expect.assertions(1);
+    test('<FixedNavBar/> is being rendered',  () => {
+        const nintendologo = screen.getByTestId('nintendo_logo')
+        expect(nintendologo).toBeInTheDocument();
     })
 })
