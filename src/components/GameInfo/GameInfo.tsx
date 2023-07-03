22a4cbd6b43dab93b5b827d2006f66ceb46dcafb
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-07-02 21:59:19 -0400

    added read more functionality

--- src/components/GameInfo/GameInfo.tsx
+++ src/components/GameInfo/GameInfo.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { gameDataType } from "../../type.d";
 
 type Extract = Pick<gameDataType, "infoHeader" | "information">
@@ -7,14 +8,28 @@ interface Props {
 
 export const GameInfo: React.FC<Props> = ({info}): JSX.Element => {
     const {infoHeader, information} = info;
+    const [heigth, setHeigth] = useState<'h-[280px]' | 'h-fit'>('h-[280px]')
     
     return(
         <section className="grid grid-cols-2 justify-center items-center w-[1000px] mx-auto mt-32 gap-4">
             <div className="">
                 <h2 className="text-main font-bold mb-4 text-2xl"> {infoHeader} </h2>
-                <p className="text-main whitespace-pre-line"> {information} </p>
+                <p className={`text-main whitespace-pre-line ${heigth} overflow-hidden`}> {information} </p>
+                <br/>
+                <button 
+                    onClick={() => setHeigth(() => heigth === 'h-fit' ? 'h-[280px]' : 'h-fit')} 
+                    className="text-secundary font-bold hover:text-red-700"
+                >
+                    Read More
+                </button>
+                <p className="text-xs text-main">
+                Software description provided by the publisher.
+                </p>
+                <button className="bg-secundary w-8/12 h-12 text-white text-lg font-semibold rounded-md mt-6 hover:scale-110 transition duration-200 hover:bg-red-700"> 
+                Explore this game is oficial website 
+                </button>
             </div>
-            <div className="">
+            <div className="self-start">
                 <img src={require('../../assets/hero.avif')} alt="main" className="w-full"/>
             </div>
         </section>
