cb4549405a89fe9442cef128acbf0515951f779a
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-07-04 22:38:19 -0400

    bottom navbar squeleton built

--- src/components/FixedNavBar/FixedNavBar.tsx
+++ src/components/FixedNavBar/FixedNavBar.tsx
@@ -22,12 +22,12 @@ export const FixedNavBar = (): JSX.Element => {
     return(
         <header className="fixed top-0 left-0 right-0 z-10">
             { viewportWidth < 1024 
-                ? <MediumNavBar/>
-                : 
+                ? 
                 <>
-                    <LargeNavBar/>
+                    <MediumNavBar/>
                     <BottomNavBar/>
                 </>
+                : <LargeNavBar/>
             }
             <div className="bg-white flex justify-center gap-8 items-center w-full h-[52px] border-b-[1px] border-main max-[1024px]:hidden">
                 <button className="h-5/6 pt-3 text-main font-bold hover:text-secundary transition ease-in-out duration-200"> My Nintendo Store </button>
