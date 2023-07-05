26386127805e8d1c16cfd06e2967e02e15e95389
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-07-04 22:04:33 -0400

    responsive navbar resoved

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Creating a nintendo website clone (game purchase site)
 
 ### Week July 3rd - July 7th
-- [ ] NavBar responsive breakpoint at 1024px
+- [x] NavBar responsive breakpoint at 1024px
 - [ ] GameDisplay and GameInfo responsive breakpoint at 768px
 - [ ] GameDetails responsive breakponit at 768px 
 - [ ] Footer breakpoint at 768px and 1024px
@@ -15,7 +15,7 @@
         dateFormat MM-DD
         axisFormat %m-%d
         section FrontEnd
-            NavBar BP 1024px                :active, 07-04, 1d
+            NavBar BP 1024px                :done, 07-04, 1d
             GameDisplay/Info BP 768px       : 07-05, 1d
             GameDetails BP 768px            : 07-06, 1d
             Footer BP 768px/1024px          : 07-07, 1d
--- src/components/FixedNavBar/FixedNavBar.tsx
+++ src/components/FixedNavBar/FixedNavBar.tsx
@@ -1,11 +1,29 @@
-import { NavBar1024px } from "./NavBar1024px"
+import { useEffect, useState } from "react"
+import { MediumNavBar } from "./MediumNavBar";
+import { LargeNavBar } from "./LargeNavBar";
+
 
 export const FixedNavBar = (): JSX.Element => {
+    const [viewportWidth, setViewportWidth] = useState(window.innerWidth)
+    
+    function setWindowDimensions(){
+        setViewportWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', setWindowDimensions);
+        
+        return () => {
+          window.removeEventListener('resize', setWindowDimensions)
+        }
+      }, [])
+
     return(
         <header className="fixed top-0 left-0 right-0 z-10">
-            <div className="bg-white flex">
-                <NavBar1024px/>
-            </div>
+            { viewportWidth < 1024 
+                ? <MediumNavBar/>
+                : <LargeNavBar/>
+            }
             <div className="bg-white flex justify-center gap-8 items-center w-full h-[52px] border-b-[1px] border-main max-[1024px]:hidden">
                 <button className="h-5/6 pt-3 text-main font-bold hover:text-secundary transition ease-in-out duration-200"> My Nintendo Store </button>
                 <button className="h-5/6 pt-3 text-main font-bold hover:text-secundary transition ease-in-out duration-200"> Games </button>
