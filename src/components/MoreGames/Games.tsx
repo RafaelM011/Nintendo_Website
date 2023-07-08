c067de3bdc79c818c2fb3b7a6456b8c3301ccfeb
Rafael Martinez <rafaelmartinez011@gmail.com>
2023-07-08 18:04:04 -0400

    more games slider

--- src/components/MoreGames/Games.tsx
+++ src/components/MoreGames/Games.tsx
@@ -2,10 +2,10 @@ import noImage from "../../assets/no_image.webp"
 
 export const Games: React.FC = (): JSX.Element => {
     return(
-        <div className="w-4/12 h-[350px] border-main border-[1px]">
+        <div className="w-fit h-[350px] border-main border-[1px]">
             <img className="w-full h-2/5" src={noImage} alt='no-img'/>
             <div className="py-4 px-4 h-3/5 flex flex-col justify-between">
-                <div className="">
+                <div className="w-[200px]">
                     <h1 className="text-[#484848] font-semibold"> Game Title 1 </h1>
                     <p className="text-main"> game release date </p>
                 </div>
